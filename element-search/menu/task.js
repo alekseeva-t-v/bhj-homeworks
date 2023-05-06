8a4ffddafb172e34f66e54a195ce1bac8852d64a
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-05-06 10:23:04 +0300

    modiffed task solution menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,15 +3,20 @@ const menuLinks = document.querySelectorAll('.menu__link');
 menuLinks.forEach((menuLinkElem) => {
   const menuItem = menuLinkElem.closest('.menu__item');
   const menuMain = menuLinkElem.closest('.menu_main');
+  const menuElem = menuItem.querySelector('.menu')
 
-  if (menuItem.querySelector('.menu')) {
+  if (menuElem) {
     menuLinkElem.addEventListener('click', function (event) {
+      if (menuElem.classList.contains('menu_active')) {
+        menuElem.classList.remove('menu_active');
+        return;
+      }
       const menuList = menuMain.querySelectorAll('.menu');
       menuList.forEach((menu) => {
         menu.classList.remove('menu_active');
       });
       event.preventDefault();
-      menuItem.querySelector('.menu').classList.add('menu_active');
+      menuElem.classList.add('menu_active');
     });
   }
 });
