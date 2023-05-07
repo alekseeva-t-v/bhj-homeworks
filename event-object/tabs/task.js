42a9886d20d86da8156796451527576528ab2201
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-05-07 10:42:35 +0300

    task tabs completed

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -6,10 +6,13 @@ tabList.forEach((tabItem, index) => {
     tabList.forEach((tabItem) => {
       tabItem.classList.remove('tab_active');
     });
+
     tabItem.classList.add('tab_active');
+
     tabContentsList.forEach((tabContentItem) => {
       tabContentItem.classList.remove('tab__content_active');
     });
+    
     tabContentsList[index].classList.add('tab__content_active');
   });
 });
