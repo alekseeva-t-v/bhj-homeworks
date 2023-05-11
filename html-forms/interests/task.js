6fce232692816c4903c3052b2e373c31576ac74c
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-05-11 11:16:58 +0300

    task interests completed

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,27 +1,43 @@
 const interestCheckList = document.querySelectorAll('.interest__check');
 
 interestCheckList.forEach((interestCheck) => {
-  interestCheck.addEventListener('change', (event) => {
-    if (!interestCheck.closest('.interests_active')) {
-      const interest = interestCheck.closest('.interest');
-      const interestsActive = interest.querySelector('.interests_active');
+  interestCheck.addEventListener('change', () => {
+    const currentInterestElem = interestCheck.closest('.interest');
+    const parrentInterestElem =
+      currentInterestElem.parentElement.closest('.interest');
+    const childElemList = Array.from(currentInterestElem.childNodes);
+
+    if (childElemList.length > 3) {
+      const interestsActive =
+        currentInterestElem.querySelector('.interests_active');
       const currentInterestCheckList =
         interestsActive.querySelectorAll('.interest__check');
-      if (interestCheck.checked) {
-        currentInterestCheckList.forEach((currentInterestCheck) => {
-          currentInterestCheck.checked = true;
-        });
-      } else {
-        currentInterestCheckList.forEach((currentInterestCheck) => {
-          currentInterestCheck.checked = false;
-        });
-      }
+      currentInterestCheckList.forEach((currentInterestCheck) => {
+        currentInterestCheck.checked = interestCheck.checked ? true : false;
+      });
     }
-  });
-});
 
-interestCheckList.forEach((interestCheck) => {
-  interestCheck.addEventListener('change', (event) => {
-    
+    if (parrentInterestElem) {
+      const list = parrentInterestElem.getElementsByTagName('ul')[0];
+      const interestCheckList = Array.from(
+        list.querySelectorAll('.interest__check')
+      );
+      const parrentElem = parrentInterestElem.querySelector('.interest__check');
+      let isAllElementsActive = [];
+
+      interestCheckList.forEach((interestCheck) => {
+        if (interestCheck.checked) {
+          isAllElementsActive.push('true');
+        } else {
+          isAllElementsActive.push('false');
+        }
+      });
+
+      const everyTrue = isAllElementsActive.every((value) => {
+        return value === 'true';
+      });
+      parrentElem.checked = everyTrue ? true : false;
+      parrentElem.indeterminate = everyTrue ? false : true;
+    }
   });
 });
