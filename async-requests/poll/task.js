36643412dcdfe8f6db481c57ebc0850e9d47210c
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-05-19 06:43:40 +0300

    Completed advanced task level Poll

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -4,18 +4,22 @@ const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.modal__close');
 
-let xhr = new XMLHttpRequest();
+let voteIndex = 0;
+let answerIndex = 0;
+
+const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
 xhr.send();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4) {
     let res = JSON.parse(xhr.responseText);
-    let data = res['data'];
+    voteIndex = res.id;
+    let data = res.data;
 
-    pollTitle.innerHTML = data['title'];
+    pollTitle.innerHTML = data.title;
 
     let answersOutput = '';
-    data['answers'].forEach((answer) => {
+    data.answers.forEach((answer) => {
       answersOutput += `
       <button class="poll__answer">
         ${answer}
@@ -26,7 +30,12 @@ xhr.onreadystatechange = function () {
 
     const pollAnswerList = document.querySelectorAll('.poll__answer');
     pollAnswerList.forEach((pollAnswer) => {
-      pollAnswer.addEventListener('click', () => {
+      pollAnswer.addEventListener('click', (event) => {
+        event.preventDefault();
+        answerIndex = data.answers.findIndex((answer) => {
+          return event.target.textContent.trim() === answer;
+        });
+
         modal.classList.add('modal_active');
         overlay.classList.add('overlay_active');
       });
@@ -37,4 +46,31 @@ xhr.onreadystatechange = function () {
 modalClose.addEventListener('click', () => {
   modal.classList.remove('modal_active');
   overlay.classList.remove('overlay_active');
+
+  const xhrRes = new XMLHttpRequest();
+  xhrRes.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
+  xhrRes.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhrRes.send(`vote=${voteIndex}&answer=2`);
+
+  xhrRes.onreadystatechange = function () {
+    if (xhrRes.readyState === 4) {
+      let votingResults = JSON.parse(xhrRes.responseText).stat;
+      console.log(votingResults);
+      let resultOutput = '';
+      let votesCount = 0;
+      votingResults.forEach((result) => {
+        votesCount += result.votes;
+      });
+
+      votingResults.forEach((result) => {
+        percentageRes = ((result.votes / votesCount) * 100).toFixed(2);
+        resultOutput += `
+                                <p>
+                                    ${result.answer}: <b>${percentageRes}%</b>
+                                </p>`;
+      });
+
+      pollAnswers.innerHTML = resultOutput;
+    }
+  };
 });
