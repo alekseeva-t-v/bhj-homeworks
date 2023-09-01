7a5ed80753241547731feaef2410ce8240e97f29
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-09-01 06:58:58 +0300

    refactor: change structure Keysolo task

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -11,12 +11,20 @@ class Game {
     this.registerEvents();
   }
 
+  /**
+   * Сброс игры (1) Запускает новую игру. (2) Сбрасывает счетчики правильно и неправильно введенных слов
+   *
+   */
   reset() {
     this.setNewWord();
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
 
+  /**
+   * Регистрирует событие нажатия кнопки на клавиатуре (1) Сохраняет в переменные введенный с клавиатуры символ  и текущий символ строки, предварительно приводит их к нижнему регистру. (2) Сравивает символы, если одинаковы запускает метод success, иначе fail
+   *
+   */
   registerEvents() {
     document.addEventListener('keypress', (event) => {
       const enteredSymbol = event.key.toLowerCase();
@@ -29,6 +37,10 @@ class Game {
     });
   }
 
+  /**
+   * Следит за таймером игры (1) Уменьшает текстовое отображение таймера на единицу. (2) Если таймер становится равным 0 запускает метод fail
+   *
+   */
   countTime = () => {
     this.timerElement.textContent--;
     if (Number(this.timerElement.textContent) === 0) {
@@ -36,9 +48,15 @@ class Game {
     }
   };
 
+  /**
+   * Обрабатывает поведение после успешного ввода символа (1) Удаляет у элемента класс, помечающий его, как выбранный (2) Добавляет класс, помечающий элемент, как корректно введенный (3) Переходит к следующему элементу, делая его текущим (3) Если такой символ существкет, присваиваем ему соответствующий класс и выходим из метода (4) Иначе, если счетчик правильно введенных слов + 1 стал равен 10, объявляем победу, вызываем метод reset для сброса игры (5) Вызывем метод setNewWord, отображающий новое слово / фразу
+   *
+   */
   success() {
-    if (this.currentSymbol.classList.contains('symbol_current'))
+    if (this.currentSymbol.classList.contains('symbol_current')) {
       this.currentSymbol.classList.remove('symbol_current');
+    }
+
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
 
@@ -51,9 +69,14 @@ class Game {
       alert('Победа!');
       this.reset();
     }
+
     this.setNewWord();
   }
 
+  /**
+   * Обрабатывает поведение после неверного ввода символа (1) Если счетчик неправильно введенных слов + 1 стал равен 5, объявляем поражение, вызываем метод reset для сброса игры (2) Вызывем метод setNewWord, отображающий новое слово / фразу
+   *
+   */
   fail() {
     if (++this.lossElement.textContent === 5) {
       alert('Вы проиграли!');
@@ -62,6 +85,10 @@ class Game {
     this.setNewWord();
   }
 
+  /**
+   * Отвечает за сохранение новой фразы / слова (1) Выбирает фразу / слово с помощью метода getWord и сохраняет ее в переменную (2) Передает сохраненную фразу / слово в метод renderWord, формирующий необходимую разметку (3) Указываем в текстовом содержимом таймера длительность равную количеству символов в слове / фразе (4) Очищаем интервал и создаем новый, используя метод countTime, который будет вызываться каждую секунду
+   *
+   */
   setNewWord() {
     const word = this.getWord();
 
@@ -74,6 +101,11 @@ class Game {
     this.intervalId = setInterval(this.countTime, 1000);
   }
 
+  /**
+   * Возвращает выбранное случайным образом из массива слово или фпазу.
+   *
+   * @return {string} выбранная фраза.
+   */
   getWord() {
     const words = [
         'All we need это любовь',
@@ -93,11 +125,18 @@ class Game {
     return words[index];
   }
 
+  /**
+   * Формирует разметку для выбранной фразы
+   *
+   * @param {string} word выбранная фраза.
+   */
   renderWord(word) {
     const html = [...word]
       .map(
-        (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
+        (simbol, index) =>
+          `<span class="symbol ${
+            index === 0 ? 'symbol_current' : ''
+          }">${simbol}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
