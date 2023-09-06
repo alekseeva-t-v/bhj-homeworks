c890f9a6cc53a1721bf4c502dc3131137a224584
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-09-06 11:54:17 +0300

    refactor: change structure Todo task

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,10 +2,19 @@ const tasksInput = document.querySelector('.tasks__input');
 const tasksAddButton = document.querySelector('.tasks__add');
 const tasksList = document.querySelector('.tasks__list');
 let tasks;
+
 !localStorage.tasks
   ? (tasks = [])
   : (tasks = JSON.parse(localStorage.getItem('tasks')));
 
+/**
+ * Создает и возвращает элемент согласно заданным параметрам.
+ *
+ * @param {string} tag Тег формируемого элемента
+ * @param {string} className Название класса элемента.
+ * @param {object} parentElement Родительский элемент.
+ * @return {object} Созданный элемент.
+ */
 function createNewElement(tag, className, parentElement) {
   const element = document.createElement(tag);
   element.className = className;
@@ -14,6 +23,11 @@ function createNewElement(tag, className, parentElement) {
   return element;
 }
 
+/**
+ * Создает элемент задачи.
+ *
+ * @param {string} text Текст задачи
+ */
 function createTaskElement(text) {
   const task = createNewElement('div', 'task', tasksList);
   const taskTitle = createNewElement('div', 'task__title', task);
@@ -35,6 +49,9 @@ function createTaskElement(text) {
   });
 }
 
+/**
+ * Добавляет новую задачу
+ */
 function addTask() {
   createTaskElement(tasksInput.value.trim());
   tasks.push(tasksInput.value.trim());
@@ -42,10 +59,16 @@ function addTask() {
   updateLocal();
 }
 
+/**
+ * Обновляет локальное хранилище
+ */
 function updateLocal() {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+/**
+ * Формирует список задач
+ */
 function fillHtmlList() {
   if (tasks.length > 0) {
     tasks.forEach((item) => {
@@ -59,12 +82,12 @@ fillHtmlList();
 tasksAddButton.addEventListener('click', (event) => {
   event.preventDefault();
   if (tasksInput.value.trim()) {
-    addTask()
+    addTask();
   }
 });
 
 document.addEventListener('keyup', (event) => {
   if (event.key === 'Enter' && tasksInput.value.trim()) {
-    addTask()
+    addTask();
   }
 });
